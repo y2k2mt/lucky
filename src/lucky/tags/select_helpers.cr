80f290a05696a95d560d9b8236a6e43cab881f92
Paul Smith <paulcsmith0218@gmail.com>
2017-12-13 21:46:09 -0500

    Make options_for_select generic
    Closes #292
    
    This works, the problem is some Fields are nilable, which would mean the
    second item in the tuple would be nilable. That's probably not what the
    user wants. On the other hand, it seems unlikely that a `nil` option
    would end up there.

--- src/lucky/tags/select_helpers.cr
+++ src/lucky/tags/select_helpers.cr
@@ -1,15 +1,13 @@
 module Lucky::SelectHelpers
-  alias SelectOption = (String | Int32 | Int64)
-
   def select_input(field : LuckyRecord::AllowedField, **html_options)
     select_tag merge_options(html_options, {"name" => input_name(field)}) do
       yield
     end
   end
 
-  def options_for_select(field : LuckyRecord::AllowedField, select_options : Array(Tuple(String, SelectOption)), **html_options)
+  def options_for_select(field : LuckyRecord::AllowedField(T), select_options : Array(Tuple(String, T)), **html_options) forall T
     select_options.each do |option_name, option_value|
-      attributes = { "value" => option_value.to_s }
+      attributes = {"value" => option_value.to_s}
 
       is_selected = option_value.to_s == field.param.to_s
       attributes["selected"] = "true" if is_selected
